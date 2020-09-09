fbf2fff4c43bf5dbde0a51354692f85855ae5419
Rudi Farkas <rudi.farkas@gmail.com>
2020-09-09 14:08:20 +0200

    index.js: add imports; yarn add @apollo-client graphql: builds, runs as initial, shows rotating logo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+import { ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
