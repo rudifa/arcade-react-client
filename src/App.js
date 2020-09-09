5af21f469a641f882ccf7771f1965e11bc8867ed
Rudi Farkas <rudi.farkas@gmail.com>
2020-09-09 14:10:57 +0200

    fix localhost address 'http://localhost:4000/graphql'; add React imports and use them

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
+import { useQuery, gql } from '@apollo/client';
+
+const GET_BOOKS = gql` {
+  books { 
+    title
+    rating
+    author
+  }
+}
+`
+
+const Book = ({ book: { title, author } }) => (
+  <div className='Card'>
+      <div>
+        <br/>
+        <h3 className='Card--name'>{title} </h3> by {author} 
+      </div>
+  </div>
+)
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const { loading, error, data } = useQuery(GET_BOOKS)
+
+    if (error) {
+      console.error("***", error);
+      return <h1>Something went wrong!</h1>
+    }
+    if (loading) return <h1>Loading...</h1>
+
+    return (
+        <main className='App'>
+            <h1>D-Arcade Books</h1>
+            {data.books.map((book) => (
+                <Book key={book.author} book={book} />
+            ))}
+        </main>
+    )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -8,9 +8,16 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache()
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
